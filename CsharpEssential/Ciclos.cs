9db438b799d4ceb07aad293ff5dc4ebf7b3c79d6
Miguel Chin-xay <miguel.peru.seo@gmail.com>
2017-07-09 22:29:21 -0500

    se agrego ciclos

---
+++ CsharpEssential/Ciclos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CsharpEssential
+{
+    public class Ciclos
+    {
+        //CICLOS O ITERACIONES
+        /*
+         * e.g: for, while, do-while
+         */
+
+        public void Main(string[] args)
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                Console.WriteLine("9*" + i + "= " + (9 * i));
+            }
+
+        }
+    }
+}
--- CsharpEssential/Program.cs
+++ CsharpEssential/Program.cs
@@ -154,6 +154,7 @@ namespace CsharpEssential
                     break;
 
             }
+			
             Console.ReadKey();
         }
     }
